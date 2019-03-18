43e3046b8406d8a17901fdc0310ac46113d91ddc
Natalia PN <npelegero@gmail.com>
2019-03-18 18:01:27 +0100

    Add server request and show rooms list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
+import { get } from 'https';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      homecards: []
+    }
+  }
+
+  componentDidMount() {
+    this.getRooms();
+  }
+
+  getRooms() {
+     fetch ('/api/homecards')
+      .then(response => response.json())
+      .then(data => {console.log(data)
+        const homeResults = data.homecards;
+
+        this.setState({homecards: homeResults})
+      })
+  }
+
+
+
+ 
+
+
   render() {
+
+    const { homecards } = this.state;
     return (
       <div className="App">
         <header className="App__header">
@@ -15,20 +45,25 @@ class App extends Component {
         </header>
 
         <main className="Main-section">
-          <div className="Room-card__container">
-            <img className="Room__image"></img>
+          <ul className="Rooms__list">
+            {homecards.map((card, index)=> {
+              return(
+              <li className="Room-card__container" key={index}>
+                <img className="Room__image" src={card.photoUrls.homecardHidpi} alt={card.title}></img>
 
-            <div className="Room__info">
-              <p className="Room__description"></p>
-              <p className="Room__price"></p>
-            </div>
+                <div className="Room__info">
+                  <p className="Room__description">{card.title}</p>
+                  <p className="Room__price">{`${card.pricePerMonth}€`}</p>
+                </div>
 
-            <div className="Button__container">
-              <button type="Button" className="Details__button">More Details</button>
-              <button type="Button" className="Book__button">Book now!</button>
-            </div>
-          
-          </div>
+                <div className="Button__container">
+                  <button type="Button" className="Details__button">More Details</button>
+                  <button type="Button" className="Book__button">Book now!</button>
+                </div>
+              </li>
+
+            )})}
+         </ul>
         </main>
       </div>
     );
