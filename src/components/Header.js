87fb2bd5ad7b6b5d24280366632448d8e24b29da
Natalia PN <npelegero@gmail.com>
2019-03-19 16:13:23 +0100

    Remove pseudo-elements for menu divider bars and add span elements

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ class Header extends Component {
             <Fragment>
                 <p className="Company__name">Spotaroom</p>
                 <nav className="App__menu">
-                    <a className="Company-info" href="">The company</a>
-                    <a className="Working-info" href="">How we work</a>
+                    <a className="Company-info" href="">The company</a><span>-</span>
+                    <a className="Working-info" href="">How we work</a><span>-</span>
                     <a className="Contact-info" href="">Contact us</a>
                 </nav>
             </Fragment>
