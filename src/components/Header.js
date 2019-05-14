d03ffb9a2f8417ea3e523cbf111f370c13cc9aa1
Natalia PN <npelegero@gmail.com>
2019-05-14 08:49:07 +0200

    Add urls to navigation menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,10 @@ class Header extends Component {
         return(
             <Fragment>
                 <p className="Company__name">Spotaroom</p>
-                
                 <nav className="App__menu">
-                    <a className="Company-info" href="">The company</a><span>-</span>
-                    <a className="Working-info" href="">How we work</a><span>-</span>
-                    <a className="Contact-info" href="">Contact us</a>
+                    <a className="Company-info" href="https://www.spotahome.com/about-us">The company</a><span>-</span>
+                    <a className="Working-info" href="https://rent.spotahome.com/work-with-us">How we work</a><span>-</span>
+                    <a className="Contact-info" href="https://www.spotahome.com/contact-us">Contact us</a>
                 </nav>
             </Fragment>
          );
